bdb795062f072474bc403b4ef34088c9666dbfd3
Rahul Ahire <info@rahulahire.com>
2025-04-17 19:56:05 +0530

    File Upload works well locally, proceeding to test it on remote machine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::DefaultBodyLimit, routing::{get, post}, serve, Router};
+mod upload_blend_file;
+
+
+async fn hello_world() -> &'static str {
+    "hello_world"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+    .route("/", get(hello_world))
+    .route("/upload_blend_file", post(upload_blend_file::upload_blend_file_handler))
+    .layer(DefaultBodyLimit::max(20 * 1024 * 1024 *1024));
+
+
+
+    
+    let listner = tokio::net::TcpListener::bind("localhost:3000")
+        .await
+        .unwrap();
+    serve(listner, app).await.unwrap();
 }
