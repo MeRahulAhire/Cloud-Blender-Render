c482223d767d2e4633918ee041e920668735b665
Rahul Ahire <info@rahulahire.com>
2025-05-30 02:09:55 +0530

    Debugged and fixed the alignment issues

--- frontend/src/component/Controlpanel.jsx
+++ frontend/src/component/Controlpanel.jsx
@@ -1,8 +1,12 @@
+import "../style/controlpanel.css"
+
 export default function Controlpanel() {
   return (
     <>
       <div className="dp-control-panel">
-        
+        {/* <div className="dp-control-panel-container">
+
+        </div> */}
       </div>
     </>
   );
