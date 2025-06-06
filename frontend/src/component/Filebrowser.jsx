970cd7a34510022de5846ba1316cf85030c45115
Rahul Ahire <info@rahulahire.com>
2025-06-06 23:17:25 +0530

    styling file list is remaining

--- frontend/src/component/Filebrowser.jsx
+++ frontend/src/component/Filebrowser.jsx
@@ -1,12 +1,28 @@
-import "../style/filebrowser.css"
+import "../style/filebrowser.css";
 
 export default function Filebrowser() {
+    const items = Array.from({ length: 1000 });
   return (
-    <div className='filebrowser-container'>
-        <div className="filelist-box">
-            
-        </div>
-
+    <div className="filebrowser-container">
+      <div className="filelist-box">
+        {/* <File_list /> */}
+        {items.map((_, index) => (
+        <File_list key={index} index={index + 1}/>
+      ))}
+      </div>
+      <div className="download-files">
+        Select folder and download all files
+      </div>
     </div>
-  )
+  );
 }
+
+const File_list = ({index}) => {
+  return (
+    <>
+      <div className="file-list">
+        <p>{String(index).padStart(4, '0')}.png</p>
+      </div>
+    </>
+  );
+};
