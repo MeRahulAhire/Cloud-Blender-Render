fc2430f84b537422929fad7265896f0eef7a934d
Rahul Ahire <info@rahulahire.com>
2025-09-30 15:42:05 +0530

    added fix for unable to delete files in Jupyterlab and disabling cache for servering freshly rendered images

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,3 @@
-// mod db;
-// mod delete_blend_file;
-// mod live_image_preview;
-// mod live_render_stats;
-// mod machine_lookup;
-// mod process_blend_file;
-// mod render_image_list;
-// mod upload_blend_file;
-// mod auth;
-
-// use axum::{
-//     Router,
-//     extract::DefaultBodyLimit,
-//     http::{HeaderValue, Method},
-//     routing::{get, get_service, post},
-//     serve,
-// };
-// // use http::{Method, header};
-// use socketioxide::{SocketIoBuilder, extract::SocketRef};
-// use tower_http::cors::CorsLayer;
-// use tower_http::services::ServeDir;
-
-// async fn hello_world() -> &'static str {
-//     "hello world"
-// }
-
-// async fn socket_handler(socket: SocketRef) {
-//     //Run your blend file
-//     process_blend_file::start_render(&socket);
-
-//     // Live Image Preview
-//     live_image_preview::live_image_preview_handler(socket.clone());
-
-//     // Blender Render Stats
-//     live_render_stats::render_stats_watcher(socket.clone());
-
-//     // Network Stats
-//     machine_lookup::network_stats(socket.clone());
-
-//     // CPU Stats
-//     machine_lookup::cpu_stats(socket.clone());
-
-//     // RAM stats
-//     machine_lookup::ram_stats(socket.clone());
-
-//     // GPU utilisation stats
-//     machine_lookup::gpu_util_stats(socket.clone());
-
-//     // GPU memory usage.
-//     machine_lookup::gpu_mem_stats(socket.clone());
-// }
-
-// #[tokio::main]
-// async fn main() {
-//     println!("🌐  Server running on Port : 4000");
-
-//     let origin = HeaderValue::from_static("http://localhost:5173");
-
-//     let cors = CorsLayer::new()
-//         .allow_origin(origin)
-//         .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
-//         .allow_headers([http::header::CONTENT_TYPE])
-//         .allow_credentials(true);
-
-//     let (socket_layer, io) = SocketIoBuilder::new().max_payload(20_000_000).build_layer();
-
-//     io.ns("/", socket_handler);
-
-//     let app = Router::new()
-//         .route("/", get(hello_world))
-//         .route(
-//             "/upload_blend_file",
-//             post(upload_blend_file::upload_handler),
-//         )
-//         .route(
-//             "/delete_blend_file",
-//             post(delete_blend_file::delete_handler),
-//         )
-//         .route("/get_db", post(db::get_app_state::get_db))
-//         .route("/stop_render", post(process_blend_file::stop_render))
-//         .route("/render_list", post(render_image_list::get_images_list))
-//         .route("/create_auth", post(auth::set_auth))
-//         .route("/delete_auth", post(auth::delete_auth))
-//         .route("/share", get(auth::share_auth))
-//         .nest_service("/images", get_service(ServeDir::new("./output")))
-//         .layer(DefaultBodyLimit::max(20 * 1024 * 1024 * 1024))
-//         .layer(socket_layer)
-//         .layer(cors)
-//         .layer(db::db_handler());
-
-//     let listner = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
-//     serve(listner, app).await.unwrap();
-// }
-
 mod auth;
 mod db;
 mod delete_blend_file;
@@ -107,15 +13,16 @@ use axum::{
     extract::DefaultBodyLimit,
     extract::Path,
     http::{HeaderValue, Method, StatusCode, header},
+    middleware,
     response::Response,
     routing::{get, get_service, post},
     serve,
-    middleware
 };
 use include_dir::{Dir, include_dir};
 use socketioxide::{SocketIoBuilder, extract::SocketRef};
 use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
+use tower_http::set_header::SetResponseHeaderLayer;
 
 // Include the dist directory at compile time
 static DIST_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/frontend/dist");
@@ -244,7 +151,13 @@ async fn main() {
         .route("/render_list", post(render_image_list::get_images_list))
         .route("/create_auth", post(auth::set_auth))
         .route("/delete_auth", post(auth::delete_auth))
-        .nest_service("/images", get_service(ServeDir::new("./output")))
+        .nest_service(
+            "/images",
+            get_service(ServeDir::new("./output")).layer(SetResponseHeaderLayer::if_not_present(
+                header::CACHE_CONTROL,
+                HeaderValue::from_static("no-cache, no-store, must-revalidate"),
+            )),
+        )
         .layer(DefaultBodyLimit::max(20 * 1024 * 1024 * 1024))
         .layer(socket_layer)
         .layer(cors)
--- start.sh
+++ start.sh
@@ -1,40 +1,3 @@
-# #!/bin/bash
-# set -e
-
-# # Ensure required folders exist
-# mkdir -p /workspace/temp /workspace/output /workspace/blend-folder /workspace/tmp_upload
-
-# # List of app binaries and folders to manage
-# FILES=("dragonfly-x86_64" "Cloud-Blender-Render" "blender" "cycles_optix_denoise_logic.py")
-
-# # Copy from /app if missing
-# for f in "${FILES[@]}"; do
-#     if [ ! -e "/workspace/$f" ]; then
-#         echo "Copying $f from /app to /workspace"
-#         cp -r "/app/$f" "/workspace/"
-#     else
-#         echo "$f already exists in /workspace"
-#     fi
-# done
-
-# # Set env variables
-# export PATH="/workspace/blender:$PATH"
-# export LD_LIBRARY_PATH="/usr/local/cuda/lib64:/usr/lib/x86_64-linux-gnu"
-# export TMPDIR=/workspace/tmp_upload
-
-# # Start DragonflyDB in background
-# /workspace/dragonfly-x86_64 --bind 0.0.0.0 --port 6379 &
-# sleep 5
-
-# # Start Cloud-Blender-Render in background
-# /workspace/Cloud-Blender-Render &
-
-# # Keep the container running
-# tail -f /dev/null
-
-# --------------------------------------------------------------
-
-
 #!/bin/bash
 set -e
 # Ensure required folders exist
@@ -95,7 +58,8 @@ jupyter lab \
     --ServerApp.token='' \
     --ServerApp.password='' \
     --ServerApp.allow_origin='*' \
-    --ServerApp.disable_check_xsrf=True &
+    --ServerApp.disable_check_xsrf=True \
+    --FileContentsManager.delete_to_trash=False &
 # Give services time to start
 sleep 5
 echo "All services started:"
